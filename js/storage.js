021b55b556470d4924663d0aa494143a8d61a224
Ayrthon Sadikrama <a.sadikrama@gmail.com>
2025-09-22 16:00:11 +0200

    changes

--- js/storage.js
+++ js/storage.js
@@ -1,11 +1,11 @@
 import { state } from "./state.js";
 
 export function saveStreams() {
-  localStorage.setItem("multistream_data", JSON.stringify(state.streams));
+  localStorage.setItem("streamsviewer_data", JSON.stringify(state.streams));
 }
 
 export function loadStreams() {
-  const stored = localStorage.getItem("multistream_data");
+  const stored = localStorage.getItem("streamsviewer_data");
   if (stored) {
     const parsed = JSON.parse(stored);
     state.streams = parsed.map((s) => ({
