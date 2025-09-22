5a1ced81492d154cd33460191d76259aa6bbe6c1
Ayrthon Sadikrama <a.sadikrama@gmail.com>
2025-09-22 15:36:23 +0200

    revert

--- js/state.js
+++ js/state.js
@@ -11,7 +11,4 @@ export const state = {
   offlineVisibleCount: 100,
   dragIndex: null,
   focusedId: null,
-  AUTO_REFRESH_MS: 60_000, // 60s feels safe for Helix rate limits
-  autoRefreshTimer: null,
-  refreshAbort: null,
 };
